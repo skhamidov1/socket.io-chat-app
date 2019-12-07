32e906daa005537984fd6f4aed2554ab07820b04
Seyfat Khamidov <seyfat123@gmail.com>
2019-12-06 22:35:07 -0500

    changing path out endpoint

--- router/router.js
+++ router/router.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.send("hi")
+router.get("/health", (req, res) => {
+    res.send("Server is running.")
 });
 
 module.exports = router
